845b0d592405a633a29aefbac008f8d6e65d6010
Patrick Rossol <patrick.rossol1@gmail.com>
2022-11-16 00:22:54 +0100

    added images

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-test = 1
-test2 = 30
+from matplotlib import pyplot as plt
+from skimage import io
 
+img = io.imread("E:\Visual\Python\CD-Damage-Detection\img\image_50334977.JPG", as_gray = True)
+
+plt.imshow (img)
